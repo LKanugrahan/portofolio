be451b57394becda706ccc9f22f2f842787ea903
Langgeng Kanugrahan <lkanugrahan@gmail.com>
2025-02-11 13:57:03 +0700

    feat:console.log ethereum

--- src/hooks/useEther.ts
+++ src/hooks/useEther.ts
@@ -12,6 +12,7 @@ const useEther = () => {
   const [owner, setOwner] = useState<string>();
   const handleConnect = async () => {
     console.log('testingan connect')
+    console.log((window as any).ethereum)
     if ((window as any).ethereum) {
       const provider = new ethers.BrowserProvider((window as any).ethereum);
       const contract = new ethers.Contract(
